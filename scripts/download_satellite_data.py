515ce7a1a4d5ac016c30a668dda5a7a243953d79
Giovani Valdrighi <giovani.valdrighi@gmail.com>
2023-03-01 12:12:04 -0300

    create function to download all the scenes metadata for ny

--- scripts/download_satellite_data.py
+++ scripts/download_satellite_data.py
@@ -1,4 +1,6 @@
+import pandas as pd
 import geopandas as gpd
+from shapely.geometry import Polygon
 from tqdm import tqdm
 import sys
 from time import time
@@ -9,8 +11,8 @@ from api import M2M
 
 
 def connect_to_m2m_api():
-    username = ""  # add your credentials
-    password = ""  # add your credentials
+    username = "giovanivaldrighi"  # add your credentials
+    password = "@ Poplio14usg"  # add your credentials
     m2m = M2M(username, password)
     return m2m
 
@@ -55,7 +57,7 @@ def download_data_test():
     print(f"Downloaded {len(downloadMetadata)} scenes in {end - start:.2f} seconds")
 
 
-def download_latest_ny():
+def search_all_scenes_ny():
     """Download the lastest scens for New York"""
     logging.basicConfig(
         level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
@@ -66,7 +68,8 @@ def download_latest_ny():
     msa_shp = msa_shp.to_crs("EPSG:4326")
 
     m2m = connect_to_m2m_api()
-
+    scenes_results = []
+    # search all the scenes from "high_res_ortho"
     for p in msa_shp.iloc[0]["geometry"].geoms:
         coords = [list(p.exterior.coords)]
         # config search
@@ -74,19 +77,79 @@ def download_latest_ny():
             "datasetName": "high_res_ortho",
             "geoJsonType": "Polygon",
             "geoJsonCoords": coords,
-            "maxResults": 49999,
-            "startDate": "2014-01-01",
-            "endDate": "2014-12-31",
+            "maxResults": 49999.0,
         }
-
-        download_dir = clean_dir_name(msa_shp.iloc[0]["NAME"])
         scenes = m2m.searchScenes(**search_params)
 
-        start = time()
-        downloadMetadata = m2m.retrieveScenes(
-            "high_res_ortho", scenes, download_dir=download_dir
-        )
-        end = time()
+        for r in scenes["results"]:
+            scenes_results.append(
+                {
+                    "dataset_name": "high_res_ortho",
+                    "entity_id": r["entityId"],
+                    "spatial_coverage": r["spatialCoverage"]["coordinates"],
+                    "start_date": r["temporalCoverage"]["startDate"],
+                    "end_date": r["temporalCoverage"]["endDate"],
+                }
+            )
+
+    # search all the scenes from "high_res_ortho"
+    for p in msa_shp.iloc[0]["geometry"].geoms:
+        coords = [list(p.exterior.coords)]
+        # config search
+        search_params = {
+            "datasetName": "naip",
+            "geoJsonType": "Polygon",
+            "geoJsonCoords": coords,
+            "maxResults": 49999,
+        }
+        scenes = m2m.searchScenes(**search_params)
+
+        for r in scenes["results"]:
+            scenes_results.append(
+                {
+                    "dataset_name": "naip",
+                    "entity_id": r["entityId"],
+                    "spatial_coverage": r["spatialCoverage"]["coordinates"],
+                    "start_date": r["temporalCoverage"]["startDate"],
+                    "end_date": r["temporalCoverage"]["endDate"],
+                }
+            )
+
+    scenes_results = pd.DataFrame(scenes_results)
+    # some cleaning on the dataframe and printing results
+    scenes_results["geometry"] = scenes_results["spatial_coverage"].apply(
+        lambda x: Polygon(x[0])
+    )
+    # convert start_date and end_date to datetime
+    scenes_results["start_date"] = pd.to_datetime(scenes_results["start_date"].apply(lambda x : x[:10]))
+    scenes_results["end_date"] = pd.to_datetime(scenes_results["end_date"].apply(lambda x : x[:10]))
+    print("Total of scenes for New York:", len(scenes_results))
+    print("Distribution between the two datasets:")
+    print(scenes_results["dataset_name"].value_counts())
+    print("Distribution between the years for high_res_ortho:")
+    print(
+        scenes_results[scenes_results["dataset_name"] == "high_res_ortho"][
+            "start_date"
+        ].dt.year.value_counts()
+    )
+    print("Distribution between the years for naip:")
+    print(
+        scenes_results[scenes_results["dataset_name"] == "naip"][
+            "start_date"
+        ].dt.year.value_counts()
+    )
+
+    # convert to geodataframe and save
+    scenes_results = gpd.GeoDataFrame(
+        scenes_results, geometry="geometry", crs="EPSG:4326"
+    )
+    scenes_results = scenes_results.drop(columns=["spatial_coverage"])
+    # return date columns to string
+    scenes_results["start_date"] = scenes_results["start_date"].astype(str)
+    scenes_results["end_date"] = scenes_results["end_date"].astype(str)
+    scenes_results.to_file("../data/ny_scenes.shp")
+
+    return
 
 
 def download_data():
@@ -136,4 +199,4 @@ def estimate_size():
 if __name__ == "__main__":
     # download_data_test()
     # estimate_size()
-    download_latest_ny()
+    search_all_scenes_ny()
