6633b5302aa3d2705e11a43ed6426aead9dd0c39
Giovani Valdrighi <giovani.valdrighi@gmail.com>
2023-05-12 09:03:22 -0400

    add bigger experiment

--- scripts/experiments.py
+++ scripts/experiments.py
@@ -11,18 +11,18 @@ from utils import save_reconstruction_results, get_embeddings, cluster_embedding
 
 def small_experiment():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    filenames = get_filenames()[:200000]
+    filenames = get_filenames()[:400000]
     dataset = get_sample_patches_dataset(filenames=filenames, resize=(28, 28))
     dl = DataLoader(dataset, batch_size=256, shuffle=True)
 
-    print("Training AutoEncoder ...")
-    print("===================================")
     print(f"Dataset shape: {len(dataset)}")
+    print("Training AutoEncoder ...")
+    print("===================================")    
 
-    model = SmallAutoEncoder(20, layers_per_block=4).to(device)
+    model = SmallAutoEncoder(50, layers_per_block=4).to(device)
 
     print(
-        f"Nº parameters: {sum(p.numel() for p in model.parameters() if p.requires_grad)//1000000:2f}M"
+        f"Nº parameters: {sum(p.numel() for p in model.parameters() if p.requires_grad)/1000000:.2f}M"
     )
 
     loss = nn.MSELoss()
@@ -51,12 +51,12 @@ def small_experiment():
     encoder = model.encoder
 
     model = DEC(
-        n_clusters=10, embedding_dim=20, encoder=encoder, cluster_centers=centers
+        n_clusters=10, embedding_dim=50, encoder=encoder, cluster_centers=centers
     )
     model.to(device)
 
     print(
-        f"Number of parameters: {sum(p.numel() for p in model.parameters() if p.requires_grad)//1000000:.2f}M"
+        f"Number of parameters: {sum(p.numel() for p in model.parameters() if p.requires_grad)/1000000:.2f}M"
     )
 
     loss = nn.KLDivLoss(size_average=False)
@@ -78,6 +78,82 @@ def small_experiment():
     )
 
 
+def big_experiment():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    filenames = get_filenames()[:100000]
+    dataset = get_sample_patches_dataset(filenames=filenames, resize=(224, 224))
+    dl = DataLoader(dataset, batch_size=64)
+
+    print(f"Dataset shape: {len(dataset)}")
+    print("Training AutoEncoder ...")
+    print("===================================")    
+
+    model = AutoEncoder(
+        latent_dim = 50,
+        encoder_arch="resnet50",
+        encoder_lock_weights=True,
+        decoder_layers_per_block=[2, 2, 3, 3, 3],
+        decoder_enable_bn=False,
+    ).to(device)
+
+    print(
+        f"Nº parameters: {sum(p.numel() for p in model.parameters() if p.requires_grad)/1000000:.2f}M"
+    )
+
+    loss = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+
+    losses_log, batches_log = train_reconstruction(
+        model, dl, loss, optimizer, device, epochs=1
+    )
+    save_reconstruction_results(
+        "reconstruction",
+        losses_log,
+        batches_log,
+        dl,
+        model,
+        device,
+        dir="../models/AE_resnet50/",
+    )
+
+    print("Training Clustering AutoEncoder ...")
+    print("===================================")
+    print(f"Dataset shape: {len(dataset)}")
+
+    model.eval()
+    embeddings = get_embeddings(dl, model, device)
+    centers = torch.tensor(cluster_embeddings(embeddings, 10))
+    encoder = model.encoder
+
+    model = DEC(
+        n_clusters=10, embedding_dim=50, encoder=encoder, cluster_centers=centers
+    )
+    model.to(device)
+
+    print(
+        f"Number of parameters: {sum(p.numel() for p in model.parameters() if p.requires_grad)/1000000:.2f}M"
+    )
+
+    loss = nn.KLDivLoss(size_average=False)
+
+    optimizer = torch.optim.SGD(params=list(model.parameters()), lr=0.01, momentum=0.9)
+
+    losses_log, batches_log = train_clustering(
+        model, dl, loss, optimizer, device, epochs=1
+    )
+
+    save_reconstruction_results(
+        "cluster",
+        losses_log,
+        batches_log,
+        dl,
+        model,
+        device,
+        dir="../models/DEC_resnet50/",
+    )
+
+
 if __name__ == "__main__":
     np.random.seed(42)
-    small_experiment()
+    #small_experiment()
+    big_experiment()
