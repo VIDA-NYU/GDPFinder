ffcfc4a8b2e52f1b8d40c35900e10b25589d4d37
embrewer <ethanmichaelbrewer@gmail.com>
2023-06-06 19:49:34 -0400

    added shap attempt

--- scripts/supervised_models.py
+++ scripts/supervised_models.py
@@ -1,18 +1,20 @@
+import torchgeo
+#from torchgeo.models import resnet50, ResNet50_Weights
 import torch.nn as nn
 from torchvision.models import resnet50, ResNet50_Weights
 
-
 def generate_resnet(device):
-
-    # Define ResNet-50 model
+    
+    # Define ResNet-50 model with pre-trained weights
+    #model = resnet50(weights=ResNet50_Weights.SENTINEL2_RGB_MOCO, pretrained=True)
     model = resnet50(weights=ResNet50_Weights.DEFAULT)
-    num_features = model.fc.in_features
 
     # Freeze the base model parameters
     for param in model.parameters():
         param.requires_grad = False
 
     # Add additional fully connected layers
+    num_features = model.fc.in_features
     model.fc = nn.Sequential(
         nn.Linear(num_features, 1024),
         nn.ReLU(inplace=True),
