b6ada3f1e034b7fad7e21a0b7c302ba66b1b87bf
Giovani Valdrighi <giovani.valdrighi@gmail.com>
2023-06-20 09:23:18 -0400

    add func that separate patches by city

--- scripts/download_from_ssh.py
+++ scripts/download_from_ssh.py
@@ -1,13 +1,15 @@
 import paramiko
-import numpy as np
+import geopandas as gpd
 import os
+import shutil
 from tqdm import tqdm
+
 import handle_tif_images
 import data
 
-if __name__ == "__main__":
-    vidagpu_username = ...
-    vidagpu_password = ...
+def download_patches():
+    vidagpu_username = input()
+    vidagpu_password = input()
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect('vidagpu.poly.edu', username=vidagpu_username, password=vidagpu_password)
@@ -28,17 +30,38 @@ if __name__ == "__main__":
         df.to_file("../data/output/downloaded_scenes_metadata.geojson")
 
         data.save_samples_patch(
-            output_dir = "small_patches",
-            size = 112
+            output_dir = "old_patches",
+            size = 224
         )
 
         os.remove("../data/output/unzipped_files/" + file)
-
-
-
-
-
-
     
     sftp.close()
     ssh.close()
+
+def separate_patches_by_city_state():
+    city_scenes_geojson = os.listdir("../data/scenes_metadata")
+    city_scenes_geojson = [s for s in city_scenes_geojson if s.endswith(".geojson")]
+    city_scenes_geojson.sort()
+    patches_files = os.listdir("../data/output/old_patches")
+
+    for city_scene in tqdm(city_scenes_geojson):
+        city_df = gpd.read_file("../data/scenes_metadata/" + city_scene)
+        city_state = city_scene.replace("_last_scenes.geojson", "")
+        if not os.path.exists("../data/output/patches/" + city_state):
+            os.mkdir("../data/output/patches/" + city_state)
+
+        for i, row in city_df.iterrows():
+            scene_id = row["entity_id"]
+            patches_files_scene = [p for p in patches_files if p.split("_")[0] == scene_id]
+            patches_files_scene = [p for p in patches_files_scene if os.path.exists("../data/output/old_patches/" + p)]
+            for patches_file in patches_files_scene:
+                shutil.move(
+                    "../data/output/old_patches/" + patches_file,
+                    f"../data/output/patches/{city_state}/{patches_file}"
+                )
+
+
+if __name__ == "__main__":
+    # download_patches()
+    separate_patches_by_city_state()
