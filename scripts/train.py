f3251afbcab23ebc6a9b1526a7bfcba4a19bffae
Giovani Valdrighi <giovani.valdrighi@gmail.com>
2023-06-28 09:16:36 -0400

    add test dataset for evaluation during training

--- scripts/train.py
+++ scripts/train.py
@@ -1,31 +1,43 @@
+import torch
 from models import target_distribution
 from utils import save_reconstruction_results
 from tqdm import tqdm
 
-
 def train_reconstruction(
-    model, loader, loss, optimizer, device, epochs=100, test_loader = None, dir = None, verbose=True
+    model, dl_train, dl_test, loss, optimizer, device, epochs=100, test_loader = None, dir = None, verbose=True
 ):
-    losses_log = []
-    batches_log = []
+    train_losses_log = []
+    train_batch_losses_log = []
+    test_losses_log = []
     for i in range(epochs):
         iter_loss = 0
-        for batch in tqdm(loader):
+        n = 0
+        for batch in tqdm(dl_train):
             batch = batch.to(device)
             decoded = model(batch)
             rec_loss = loss(decoded, batch)
             optimizer.zero_grad()
             rec_loss.backward()
             optimizer.step()
-            iter_loss += rec_loss.item() / batch.shape[0]
-            batches_log.append(rec_loss.item())
-        losses_log.append(iter_loss)
+            iter_loss += rec_loss.item()
+            n += batch.shape[0]
+            train_batch_losses_log.append(rec_loss.item())
+        train_losses_log.append(iter_loss / n)
 
         if verbose:
             print(f"Epoch {i+1}/{epochs} - Loss: {iter_loss:.8f}")
 
-       
-        model.eval()
+        with torch.no_grad():
+            model.eval() 
+            iter_loss = 0
+            n = 0
+            for batch in tqdm(dl_test):
+                batch = batch.to(device)
+                decoded = model(batch)
+                rec_loss = loss(decoded, batch)
+                iter_loss += rec_loss.item() 
+                n += batch.shape[0]
+            test_losses_log.append(iter_loss / n)
         save_reconstruction_results(
             "reconstruction",
             losses_log,
