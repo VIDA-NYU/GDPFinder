7b48774c1b33c05df669e2dda86b504166f69de8
Giovani Valdrighi <giovani.valdrighi@gmail.com>
2023-08-18 16:44:43 -0300

    add training of kmeans and dec

--- scripts/experiments.py
+++ scripts/experiments.py
@@ -19,19 +19,51 @@ def train_auto_encoder_extractor(dims):
     model = models.AutoEncoderResnetExtractor(
         dims=dims, denoising=True
     ).to(device)
-    # train.train_reconstruction_feature_extraction(
-    #     model,
-    #     dl_train,
-    #     dl_val,
-    #     epochs=5,
-    #     dir=f"../models/AE_extractor_resnet50_{str (dims)}/",
-    # )
-    model.load_state_dict(torch.load(f"../models/AE_extractor_resnet50_{str(dims)}/model.pt"))
+    train.train_reconstruction_feature_extraction(
+        model,
+        dl_train,
+        dl_val,
+        epochs=5,
+        dir=f"../models/AE_extractor_resnet50_{str (dims)}/",
+    )
     model.eval()
     for dl_name, dl in [("train", dl_train), ("val", dl_val), ("test", dl_test)]:
         embeddings = utils.get_embeddings(dl, model.encoder, device)
         np.save(f"../models/AE_extractor_resnet50_{str(dims)}/embeddings_{dl_name}.npy", embeddings)
 
+
+def train_kmeans_dec(dims, n_clusters):
+    dl_train, dl_val, dl_test = data.generate_dataloaders(
+        patches_count_max=50, batch_size=256
+    )
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = models.AutoEncoderResnetExtractor(
+        dims=dims, denoising=True
+    ).to(device)
+    model.load_state_dict(torch.load(f"../models/AE_extractor_resnet50_{str(dims)}/model.pt"))
+    embeddings = np.load(f"../models/AE_extractor_resnet50_{str(dims)}/embeddings_train.npy")
+
+    os.makedirs(dir, exist_ok=True)
+
+    # kmeans
+    kmeans = KMeans(n_clusters=n_clusters, random_state=0, n_init=10).fit(embeddings)
+    dir=f"../models/DEC_extractor_resnet50_{str(dims)}_{n_clusters}/",
+    joblib.dump(kmeans, f"../models/AE_extractor_resnet50_{str(dims)}/kmeans_{n_clusters}_clusters.pkl")
+
+    # dec
+    model_dec = models.DEC(
+        n_clusters=n_clusters,
+        embedding_dim=dims[-1],
+        encoder=model.encoder,
+        cluster_centers=torch.tensor(kmeans.cluster_centers_),
+    ).to(device)
+    train.train_clustering(
+        model_dec,
+        dl_train,
+        dl_val,
+        epochs=5,
+        dir=f"../models/DEC_extractor_resnet50_{str(dims)}_{n_clusters}/",
+    )
 
 def resnet_extractor_experiment(dims, n_clusters=100, train_ae = True, train_dec = True):
     dl_train, dl_val, _ = data.generate_datasets(
@@ -113,9 +145,15 @@ def resnet_extractor_experiment(dims, n_clusters=100, train_ae = True, train_dec
 if __name__ == "__main__":
     np.random.seed(42)
     train_auto_encoder_extractor([2048, 512, 128, 64])
+    train_auto_encoder_extractor([2048, 512, 128, 32])
+    train_auto_encoder_extractor([2048, 512, 128, 10])
+
+    for latent_dim in [64, 32, 10]:
+        for k in [100, 50, 20]:
+            train_kmeans_dec([2048, 512, 128, latent_dim], k)
+    
     # resnet_extractor_experiment([2048, 512, 128, 64], 100, False, True)
     # resnet_extractor_experiment([2048, 512, 128, 64], 50, False, True)
     # resnet_extractor_experiment([2048, 512, 128, 64], 20, False, True)
-    train_auto_encoder_extractor([2048, 512, 128, 32])
-    train_auto_encoder_extractor([2048, 512, 128, 10])
+    
     
